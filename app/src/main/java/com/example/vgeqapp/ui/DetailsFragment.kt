cbdd7103d6bf645add8f161d7e12d0249392c787
bapaiahmandava <bapaiahchowdary.x.mandava@kp.org>
2022-04-30 14:48:17 -0400

    update

--- app/src/main/java/com/example/vgeqapp/ui/DetailsFragment.kt
+++ app/src/main/java/com/example/vgeqapp/ui/DetailsFragment.kt
@@ -44,9 +44,9 @@ class DetailsFragment : Fragment() {
 
     fun setObservables() {
         viewmodel.getSelectedEQData.observe(viewLifecycleOwner, {
-            binding.txtMagnitude.text = getString(R.string.magnitude) + it?.magnitude
-            binding.txtDateTime.text = getString(R.string.date_and_time) + it?.date
-            binding.txtPlace.text = getString(R.string.Place) + it?.place
+            binding.txtMagnitude.text = getString(R.string.magnitude) +" "+ it?.magnitude
+            binding.txtDateTime.text = getString(R.string.date_and_time) +" "+ it?.date
+            binding.txtPlace.text = getString(R.string.Place) +" "+ it?.place
         })
     }
 
